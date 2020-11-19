259eecb2d4d3f4811395be0aa46f3c2d2a26c539
Zack Berger <zackberger@Zacks-MBP.lan>
2020-11-19 14:47:43 -0800

    test

--- unsupervised/src/pca.py
+++ unsupervised/src/pca.py
@@ -2,3 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Import data
+
+# Set random seed so that random values are deterministic
+np.random.seed(0)
+
+np.loadtxt("../data/pca_data/pca.data")
